e91c1af3c5b28afea08f4b1ba9fa8378c5be2f36
justin <jumarin22@protonmail.com>
2021-09-22 12:23:46 -0400

    Chapter 20. Hello world.

---
+++ app/src/main/java/com/raywenderlich/podplay/repository/ItunesRepo.kt
@@ -0,0 +1,8 @@
+package com.raywenderlich.podplay.repository
+
+import com.raywenderlich.podplay.service.ItunesService
+
+class ItunesRepo(private val itunesService: ItunesService) {
+    suspend fun searchByTerm(term: String) =
+        itunesService.searchPodcastByTerm(term) // 3
+}
